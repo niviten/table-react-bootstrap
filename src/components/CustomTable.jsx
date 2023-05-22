1d7cb21fbd098588929badda10a9b20fd51cb28b
niviten <l.j.nivethan@gmail.com>
2023-05-22 19:22:38 +0530

    Make table responsive Adds horizontal scroller

--- src/components/CustomTable.jsx
+++ src/components/CustomTable.jsx
@@ -16,88 +16,90 @@ function CustomTable({ tableDetails, tableRows, tableColumns }) {
       { (showSearch) ? 'show search' : 'hide search' } <span>&nbsp; - &nbsp;</span>
       { (isColumnsSearchable) ? 'columns searchable' : 'columns not searchable' }
       <hr />
-      <table className='table table-stripped table-hover table-bordered'>
-        <thead className='table-light'>
-          <tr>
-            <td colSpan={5}>
-              <button
-                className='btn btn-light'
-                data-bs-toggle='tooltip'
-                data-bs-placement='right'
-                title='Search'
-                onClick={() => setShowSearch(!showSearch)}
-              >
-                <i className="las la-search"></i>
-              </button>
-              <button
-                className='btn btn-light'
-                data-bs-toggle='tooltip'
-                data-bs-placement='right'
-                title='Delete'
-              >
-                <i className="las la-trash la-lg"></i>
-              </button>
-            </td>
-          </tr>
-        </thead>
-        <thead className='table-dark'>
-          <tr>
+      <div className='table-responsive'>
+        <table className='table table-stripped table-hover table-bordered'>
+          <thead className='table-light'>
+            <tr>
+              <td colSpan={5}>
+                <button
+                  className='btn btn-light'
+                  data-bs-toggle='tooltip'
+                  data-bs-placement='right'
+                  title='Search'
+                  onClick={() => setShowSearch(!showSearch)}
+                >
+                  <i className="las la-search"></i>
+                </button>
+                <button
+                  className='btn btn-light'
+                  data-bs-toggle='tooltip'
+                  data-bs-placement='right'
+                  title='Delete'
+                >
+                  <i className="las la-trash la-lg"></i>
+                </button>
+              </td>
+            </tr>
+          </thead>
+          <thead className='table-dark'>
+            <tr>
+              {
+                tableColumns.map((column, columnIndex) => {
+                  return (
+                    <th
+                      key={columnIndex}
+                      onClick={() => columnClickHandler(column)}
+                    >
+                      { column.columnDisplayName }
+                    </th>
+                  )
+                })
+              }
+            </tr>
+          </thead>
+          <tbody>
             {
-              tableColumns.map((column, columnIndex) => {
-                return (
-                  <th
-                    key={columnIndex}
-                    onClick={() => columnClickHandler(column)}
-                  >
-                    { column.columnDisplayName }
-                  </th>
-                )
-              })
-            }
-          </tr>
-        </thead>
-        <tbody>
-          {
-            (isColumnsSearchable && showSearch)
-            ? (
-              <tr>
-                {
-                  tableColumns.map((column, columnIndex) => {
-                    if (!column.isSearchable) {
-                      return <td key={columnIndex}>&nbsp;</td>
-                    }
-                    return (
-                      <td
-                        key={columnIndex}
-                      >
-                        <input type='text' />
-                      </td>
-                    )
-                  })
-                }
-              </tr>
-            )
-            : <></>
-          }
-          {
-            tableRows.map((row, rowIndex) => {
-              return (
-                <tr key={rowIndex}>
+              (isColumnsSearchable && showSearch)
+              ? (
+                <tr>
                   {
-                    row.map((cell, cellIndex) => {
+                    tableColumns.map((column, columnIndex) => {
+                      if (!column.isSearchable) {
+                        return <td key={columnIndex}>&nbsp;</td>
+                      }
                       return (
-                        <td key={cellIndex}>
-                          { cell }
+                        <td
+                          key={columnIndex}
+                        >
+                          <input type='text' />
                         </td>
                       )
                     })
                   }
                 </tr>
               )
-            })
-          }
-        </tbody>
-      </table>
+              : <></>
+            }
+            {
+              tableRows.map((row, rowIndex) => {
+                return (
+                  <tr key={rowIndex}>
+                    {
+                      row.map((cell, cellIndex) => {
+                        return (
+                          <td key={cellIndex}>
+                            { cell }
+                          </td>
+                        )
+                      })
+                    }
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
